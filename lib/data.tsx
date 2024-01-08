02600d136d452544fc1f327a1e6defa10930d325
James Niyongira <lascasasjames85@gmail.com>
2024-01-08 18:22:27 -0500

    updated project page and data file

--- lib/data.tsx
+++ lib/data.tsx
@@ -1,19 +1,16 @@
 export const projects = [
     {
         id: 1,
-        title: "Prescott & Russell Admin Portal",
-        description: "This is an admin portal for Prescott & Russell, built with Next.js and Tailwind CSS.",
-        image: "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?q=80&w=3044&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://jamesniyongira.com",
-        stack: [{ Framework: "Next.js" }, { Language: "TypeScript" }, { CSS: "Tailwind CSS" },{ "Use case": "Starter" }],
+        title: "Brainwave Online School",
+        description: "This is a web application that allows students to learn anything online interactively.",
+        image: "https://res.cloudinary.com/james-me/image/upload/f_auto,q_auto/v1/Portfolio-media/riynaxj7lbgelizpqbeo",
+        link: "https://brainwave-kohl.vercel.app/",
+        stack: [{ Framework: "Next.js & Django" }, { Language: "TypeScript & Python" }, { CSS: "Tailwind CSS" },{ "Use case": "Starter" }],
         // These are the steps to setup the project locally. the title for command step must always be commands
         setup: [{ title: "How it's built and what it does",
-            description:" As we've grown, we've seen how Preline has helped companies such as\n" +
-                "                                        Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their\n" +
-                "                                        designers closer together to create amazing things. We&apos;ve also learned\n" +
-                "                                        that when the culture of sharing is brought in earlier, the better\n" +
-                "                                        teams adapt and communicate with one another."
-        },{ title: "Installation guide",description:"To run this project locally on your machine, check the readme file for full instructions." }],
+            description:" This project is built with Next.js and Django. It is a web application that allows students to learn anything online interactively. It has a dashboard for teachers to manage their classes and students. It also has a dashboard for students to manage their classes and courses." +
+                "The frontend is built with Next.js and Tailwind CSS. The backend is built with Django and Django Rest Framework. The database is PostgreSQL. The project is hosted on Vercel and AWS."
+        }],
         github: "somelink.github.io",
         tag:"fullstack",
 
@@ -21,24 +18,25 @@ export const projects = [
     },
     {
         id: 2,
-        title: "My Portfolio",
-        description: "This is my portfolio website, built with Next.js and Tailwind CSS.",
-        image: "https://images.unsplash.com/photo-1519084906448-095ffc4e194d?q=80&w=3107&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://jamesniyongira.com",
-        stack: [{ Framework: "Next.js" }, { Language: "TypeScript" }, { CSS: "Tailwind CSS" },{ "Use case": "Starter" }],
-        setup: [{ title: "Here goes what the project does",description:" step description" }],
+        title: "Reactive Notes",
+        description: "This is a web application that allows users to create, read, update and delete notes.",
+        image: "https://res.cloudinary.com/james-me/image/upload/f_auto,q_auto/v1/Portfolio-media/vxqydtvs4gi5kymqjeoj",
+        link: "https://notes-app-james.netlify.app/",
+        stack: [{ Framework: "React.js" }, { Language: "JavaScript" }, { CSS: "Tailwind CSS" },{ "Use case": "Note taking" }],
+        setup: [{ title: "How it was built",description:" This app is entirely built with react and leverages react.js hooks, best practices and good design" }],
         github: "somelink.github.io",
         tag:"Frontend"
 
     },
     {
         id: 3,
-        title: "My Portfolio",
-        description: "This is my portfolio website, built with Next.js and Tailwind CSS.",
-        image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?q=80&w=3126&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://jamesniyongira.com",
+        title: "Clone of the Netflix UI",
+        description: "This is a clone of the Netflix UI built with Next.js, TypeScript, MongoDB  and Tailwind CSS.",
+        image: "https://res.cloudinary.com/james-me/image/upload/f_auto,q_auto/v1/Portfolio-media/uasxusrgcaij0ytbduyh",
+        link: "https://movies-app-james-one-green.vercel.app/",
         stack: [{ Framework: "Next.js" }, { Language: "TypeScript" }, { CSS: "Tailwind CSS" },{ "Use case": "Starter" }],
-        setup: [{ title: "Here goes what the project does",description:" step description" }],
+        setup: [{ title: "Why I cloned Netflix UI?",description:"Among the web apps that I love how they look and feel very modern, Netflix is on top tier. As a web developer it left me wondering how they made a beautiful web app like that, full of smooth animations." +
+                "I tried to replicate it so that I can learn how to design cool web UIs. " }],
         github: "somelink.github.io",
         tag:"Web API"
 
