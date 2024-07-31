320e2ae2f254ed66517be7589e43e9213a1c96c6
James Niyongira <lascasasjames85@gmail.com>
2024-07-30 22:43:23 -0400

    updated projects and contact page

--- components/SimpleLayout.jsx
+++ components/SimpleLayout.jsx
@@ -1,9 +1,15 @@
-import { Container } from '@/components/Container'
+import { Container } from "@/components/Container";
 
-export function SimpleLayout({children }) {
-    return (
-        <Container className="mt-16 sm:mt-32">
-            <div className="mt-16 sm:mt-20">{children}</div>
-        </Container>
-    )
+export function SimpleLayout({ title, intro, children }) {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <header className="max-w-2xl">
+        <h1 className="text-4xl font-bold tracking-tight text-white py-2 sm:text-5xl">
+          {title}
+        </h1>
+        <p className="my-6 text-lg text-zinc-600 dark:text-zinc-400">{intro}</p>
+      </header>
+      <div className="mt-16 sm:mt-20">{children}</div>
+    </Container>
+  );
 }
